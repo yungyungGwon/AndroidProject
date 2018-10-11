30cb2d6f0cb896f871aba677a1ec538cfe12fd63
yungyungGwon <41810920+yungyungGwon@users.noreply.github.com>
2018-10-11 18:03:37 +0900

    Android_Client1
    .

---
+++ Application_SocketClient/app/src/main/java/com/google/application_socketclient/ActivityClient.java
@@ -0,0 +1,85 @@
+package com.google.application_socketclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class ActivityClient extends AppCompatActivity {
+    private Socket socket;
+    BufferedReader in;
+    PrintWriter out;
+    EditText input;
+    Button button;
+    TextView output;
+    String data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client);
+
+        input = (EditText) findViewById(R.id.input);
+        button = (Button) findViewById(R.id.button);
+        output = (TextView) findViewById(R.id.output);
+
+        button.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                String data = input.getText().toString();
+                Log.w("NETWORK", " " + data);
+                if (data != null) {
+                    out.println(data);
+                }
+            }
+        });
+
+        Thread worker = new Thread() {
+            public void run() {
+                try {
+                    socket = new Socket("49.166.77.214", 56789);
+                    out = new PrintWriter(socket.getOutputStream(), true);                                                                                                                        //전송한다.
+                    in = new BufferedReader(new InputStreamReader(
+                            socket.getInputStream()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    while (true) {
+                        data = in.readLine();
+                        output.post(new Runnable() {
+                            public void run() {
+                                output.setText(data);
+                            }
+                        });
+                    }
+                } catch (Exception e) {
+                }
+            }
+        };
+        worker.start();
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
